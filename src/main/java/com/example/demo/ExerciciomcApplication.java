7617e55c9aaefa02b19de44910198503bb95ca4b
Willian de Freitas <willian.freitasoliveira@yahoo.com.br>
2017-09-14 22:15:01 -0300

    Criando operacao de instanciacao

--- src/main/java/com/example/demo/ExerciciomcApplication.java
+++ src/main/java/com/example/demo/ExerciciomcApplication.java
@@ -1,12 +1,32 @@
 package com.example.demo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ExerciciomcApplication {
+import com.example.demo.domain.Categoria;
+import com.example.demo.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ExerciciomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(ExerciciomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.save(Arrays.asList(cat1, cat2));
+	}
+	
+	
 }
