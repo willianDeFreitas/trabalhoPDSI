61fe60f1a5f95d3da0ac1655660aca264bd4a166
willianDeFreitas <willian.freitasoliveira@yahoo.com.br>
2017-09-21 21:52:23 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/example/demo/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.example.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.Cliente;
+import com.example.demo.repositories.ClienteRepository;
+import com.example.demo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Cliente obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+					+ ", Tipo: "+ Cliente.class.getName());
+		}
+		return obj;
+	}
+}
