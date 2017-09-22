736d3ae8149e553ea1d5df482edd90e6e5ecccbb
willianDeFreitas <willian.freitasoliveira@yahoo.com.br>
2017-09-22 17:08:19 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/example/demo/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.example.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.Pedido;
+import com.example.demo.repositories.PedidoRepository;
+import com.example.demo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Pedido obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+					+ ", Tipo: "+ Pedido.class.getName());
+		}
+		return obj;
+	}
+}
