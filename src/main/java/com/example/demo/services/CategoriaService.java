09c9dc63f44f40f5dfcfa21d12df3ada21aabba3
willianDeFreitas <willian.freitasoliveira@yahoo.com.br>
2017-10-10 22:13:00 -0300

    Correcoes: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/example/demo/services/CategoriaService.java
+++ src/main/java/com/example/demo/services/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.domain.Categoria;
+import com.example.demo.domain.Cliente;
 import com.example.demo.repositories.CategoriaRepository;
 import com.example.demo.services.exceptions.DataIntegrityException;
 import com.example.demo.services.exceptions.ObjectNotFoundException;
@@ -36,8 +37,9 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj= find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 	
 	public void delete(Integer id) {
@@ -63,5 +65,8 @@ public class CategoriaService {
 	//metodo auxiliar que instacia uma categoria a partir de um DTO
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 }
