64bb80610bf17bc8a0d5c67af3f2f1ad73939cd5
willianDeFreitas <willian.freitasoliveira@yahoo.com.br>
2017-10-19 22:21:49 -0200

    Busca de pedidos por nome e categorias - parte 1.1

---
+++ src/main/java/com/example/demo/services/ProdutoService.java
@@ -0,0 +1,42 @@
+package com.example.demo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.Categoria;
+import com.example.demo.domain.Produto;
+import com.example.demo.repositories.CategoriaRepository;
+import com.example.demo.repositories.ProdutoRepository;
+import com.example.demo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+
+	@Autowired
+	private ProdutoRepository repo;
+	
+	@Autowired
+	private CategoriaRepository categoriarepository;
+	
+	public Produto find(Integer id) {
+		Produto obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+					+ ", Tipo: "+ Produto.class.getName());
+		}
+		return obj;
+	}
+	public Page<Produto> search(String nome, List<Integer> ids,Integer page, Integer linesPerPage, String orderBy, String direction) {
+		//prepara consulta para retorno de dados
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriarepository.findAll(ids);
+		return repo.search(nome, categorias, pageRequest);
+		
+	}
+
+}
