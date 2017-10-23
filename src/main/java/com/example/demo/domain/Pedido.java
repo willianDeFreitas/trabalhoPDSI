14920498e7b8c2b07ac991071adc2410b7e947a8
willianDeFreitas <willian.freitasoliveira@yahoo.com.br>
2017-10-22 23:03:57 -0200

    Subtotal e total do Pedido

--- src/main/java/com/example/demo/domain/Pedido.java
+++ src/main/java/com/example/demo/domain/Pedido.java
@@ -28,10 +28,8 @@ public class Pedido implements Serializable {
 	@JsonFormat(pattern="dd/MM/yyyy hh:mm")
 	private Date instante;
 	
-	
 	@OneToOne(cascade = CascadeType.ALL, mappedBy="pedido")
 	private Pagamento pagamento;
-	
 	
 	@ManyToOne
 	@JoinColumn(name="cliente_id")
@@ -55,6 +53,14 @@ public class Pedido implements Serializable {
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
 
+	public double getValorTotal() {
+		double soma = 0.0;
+		for (ItemPedido ip : itens) {
+			soma = soma + ip.getSubTotal();
+		}
+		return soma;
+	}
+	
 	public Integer getId() {
 		return id;
 	}
