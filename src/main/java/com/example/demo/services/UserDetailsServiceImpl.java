f23649a7b7c6c967a83d1656e5c45b4364cc3627
willianDeFreitas <willian.freitasoliveira@yahoo.com.br>
2017-11-14 22:20:25 -0200

    Implementando autenticacao e geracao do token JWT parte 2 final

--- src/main/java/com/example/demo/services/UserDetailsServiceImpl.java
+++ src/main/java/com/example/demo/services/UserDetailsServiceImpl.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.domain.Cliente;
 import com.example.demo.repositories.ClienteRepository;
 import com.example.demo.security.UserSS;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
 	@Autowired
