dcecdb8c1dab635c67edfe25c9f4285da8b5b86b
willianDeFreitas <willian.freitasoliveira@yahoo.com.br>
2017-11-16 19:32:24 -0200

    Implementando autorizacao parte 2 final

--- src/main/java/com/example/demo/security/JWTAuthorizationFilter.java
+++ src/main/java/com/example/demo/security/JWTAuthorizationFilter.java
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -31,9 +34,21 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
 		String header = request.getHeader("Authorization");
 		
 		if (header != null && header.startsWith("Bearer")) {
-			UsernamePasswordAuthenticationToken
-			
+			UsernamePasswordAuthenticationToken auth = getAuthentication(header.substring(7));
+			if(auth != null) {
+				SecurityContextHolder.getContext().setAuthentication(auth);
+			}
 		}
+		chain.doFilter(request, response);
+	}
+
+	private UsernamePasswordAuthenticationToken getAuthentication(String token) {
+		if(jwtUtil.tokenValido(token)) {
+			String username = jwtUtil.getUsername(token);
+			UserDetails user = userDetailsService.loadUserByUsername(username);
+			return new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+		}
+		return null;
 	}
 	
 
