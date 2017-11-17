53294712b72973f0849424f7e75262f7ccb68055
willianDeFreitas <willian.freitasoliveira@yahoo.com.br>
2017-11-16 22:02:33 -0200

    Esqueci a senha

--- src/main/java/com/example/demo/services/EmailService.java
+++ src/main/java/com/example/demo/services/EmailService.java
@@ -2,6 +2,7 @@ package com.example.demo.services;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.example.demo.domain.Cliente;
 import com.example.demo.domain.Pedido;
 
 public interface EmailService {
@@ -9,4 +10,6 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
